69c6b80b73c736e43206473709d45257a1be39e5
kekoses <max_denisow68@mail.ru>
2021-11-02 22:47:10 +0300

    Implement a simple tg-bot as hosted background service

---
+++ ArgusBot/Services/Implementations/TelegramHostedService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace ArgusBot.Services.Implementations
+{
+    public class TelegramHostedService : IHostedService
+    {
+        private readonly ITelegramBotClient _client;
+        private readonly ILogger<TelegramHostedService> _logger;
+        public TelegramHostedService(ITelegramBotClient client, ILogger<TelegramHostedService> logger)
+        {
+            _client = client;
+            _logger = logger;
+        }
+        private async Task HandleUpdate(ITelegramBotClient client,Update update,CancellationToken cancelToken)
+        {
+            if (update.Type == UpdateType.Message)
+            {
+                _logger.LogInformation("Message was received!");
+                await _client.SendTextMessageAsync(update.Message.Chat.Id, update.Message.Text);
+            }   
+        }
+        private async Task HandleError(ITelegramBotClient client, Exception exception, CancellationToken cancelToken)
+        {
+            _logger.LogError(exception.Message);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _client.StartReceiving(new DefaultUpdateHandler(HandleUpdate, HandleError), cancellationToken);
+            _logger.LogInformation("Start working of bot");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                _client.StopReceiving();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
--- ArgusBot/Startup.cs
+++ ArgusBot/Startup.cs
@@ -1,13 +1,10 @@
+using ArgusBot.Services.Implementations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot;
 
 namespace ArgusBot
 {
@@ -17,29 +14,15 @@ namespace ArgusBot
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllers();
+            services.AddHttpClient("tgclient").AddTypedClient<ITelegramBotClient>(client => new TelegramBotClient(Configuration["bot-token"], client));
+            services.AddHostedService<TelegramHostedService>().AddLogging(log=>log.AddConsole());
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -50,7 +33,7 @@ namespace ArgusBot
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}");
             });
         }
     }
