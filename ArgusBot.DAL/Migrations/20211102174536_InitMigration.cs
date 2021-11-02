b165213cd76b338fe80c6a1b272428a827061ba0
Igor Prischepa <5076626+IgorPrischepa@users.noreply.github.com>
2021-11-02 21:38:19 +0300

    Added user model Enabled migrations Add SQL connection to progect

---
+++ ArgusBot.DAL/Migrations/20211102174536_InitMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ArgusBot.DAL.Migrations
+{
+    public partial class InitMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserGuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    NormalizedLogin = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelegramAccount = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserGuid);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Login",
+                table: "Users",
+                column: "Login",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- ArgusBot.DAL/Models/User.cs
+++ ArgusBot.DAL/Models/User.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ArgusBot.Models
 {
+    [Index(nameof(Login), IsUnique = true)]
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid UserGuid { get; set; }
 
+        [Required]
+        [MaxLength(40)]
         public string Login { get; set; }
 
+        [Required]
+        public string NormalizedLogin { get; set; }
+
+        [Required]
         public string Password { get; set; }
 
         public string TelegramAccount { get; set; }
