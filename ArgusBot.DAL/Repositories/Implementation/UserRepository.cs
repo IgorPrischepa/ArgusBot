08cc5b6377754df3859e10eae9a3b2a97b1a6823
Igor Prischepa <5076626+IgorPrischepa@users.noreply.github.com>
2021-11-03 13:03:01 +0300

    Implement repository for Users

---
+++ ArgusBot.DAL/Repositories/Implementation/UserRepository.cs
@@ -0,0 +1,56 @@
+﻿using ArgusBot.DAL.Repositories.Interfaces;
+using ArgusBot.Data;
+using ArgusBot.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+
+namespace ArgusBot.DAL.Repositories.Implementation
+{
+    class UserRepository : IUserRepository
+    {
+        private readonly UsersContext db;
+
+        public UserRepository(UsersContext usersContext)
+        {
+            db = usersContext;
+        }
+
+        public void Create(User user)
+        {
+            db.Users.Add(user);
+            Save();
+        }
+
+        public void Delete(Guid id)
+        {
+            User user = db.Users.First(u => u.UserGuid == id);
+            Save();
+        }
+
+        public User GetUser(Guid id)
+        {
+            return db.Users.First(u => u.UserGuid == id);
+        }
+
+        public User GetUser(string login)
+        {
+            return db.Users.First(u => u.NormalizedLogin == login.ToLower());
+        }
+
+        public User GetUserByTelegramAccount(string telegramId)
+        {
+            return db.Users.First(u => u.TelegramId == telegramId.ToLower());
+        }
+        public void Update(User user)
+        {
+            db.Entry(user).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
---
+++ ArgusBot.DAL/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using ArgusBot.Models;
+using System;
+
+namespace ArgusBot.DAL.Repositories.Interfaces
+{
+    interface IUserRepository
+    {
+        User GetUser(Guid id);
+
+        void Create(User user);
+
+        void Update(User user);
+
+        void Delete(Guid id);
+
+        void Save();
+    }
+}
