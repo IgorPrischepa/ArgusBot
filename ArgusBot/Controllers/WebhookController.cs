d5d2cb259a9ad45270a613c79e9dedb4f126f751
Igor Prischepa <5076626+IgorPrischepa@users.noreply.github.com>
2021-11-21 21:05:58 +0300

    Added webhook controller as endpoint for Telegram requests

---
+++ ArgusBot/Controllers/WebhookController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+using System.Threading.Tasks;
+using ArgusBot.Services;
+
+namespace ArgusBot.Controllers
+{
+    public class WebhookController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromServices] HandleUpdateService handleUpdateService,
+                                              [FromBody] Update update)
+        {
+
+            await handleUpdateService.EchoAsync(update);
+            return Ok();
+        }
+    }
+}
